c9095a4dc9d2eb4b2c20fe6c3ce34c755c440387
石黒誠汰 <SeitaIshiguro@SitaIshiguro.local>
2019-08-02 20:46:08 +0900

    first commit

---
+++ itunes-search-api.rb
@@ -0,0 +1,31 @@
+require "open-uri"
+require 'itunes-search-api'
+
+ITunesSearchAPI.search(
+  :term    => "東京事変",
+  :country => 'jp',
+  :media   => 'music',
+  :lang    => 'ja_jp',
+  :limit  => '2'
+).each do |item|
+  #p item
+  print "アーティスト名:"
+  puts item['artistName']
+
+  print "曲名:"
+  puts item['trackName']
+
+  print "サンプルURL:"
+  puts item['previewUrl']
+
+  print "ジャケット:"
+  puts item['artworkUrl100']
+
+  puts "-------------------------"
+
+  open(item['artworkUrl100']) {|image|
+  	File.open("#{item['trackName']}.jpg","wb") do |file|
+  		file.puts image.read
+  	end
+  }
+end
